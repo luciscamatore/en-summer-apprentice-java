b395e3873903611702d5c7f9008c9e432405a7b8
luciscamatore <sandel.delu@gmail.com>
2023-07-29 12:58:15 +0300

    changed variables, removed constructors, other fixes

--- src/main/java/com/endava/practica/controller/EventController.java
+++ src/main/java/com/endava/practica/controller/EventController.java
@@ -3,19 +3,18 @@ package com.endava.practica.controller;
 import com.endava.practica.DTO.EventDTO;
 import com.endava.practica.model.Event;
 import com.endava.practica.services.EventService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api")
 public class EventController {
 
     final EventService eventService;
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
-    }
 
     @GetMapping("/eventById")
     public EventDTO getEventByVenueIDandEventType(@RequestParam Integer venueID, @RequestParam String eventType){
